08c9ab2ae2fbc52ecf5e482208c30e94affd51f9
noatino <paranoid.tony@gmail.com>
2013-12-23 15:41:38 -0600

    Student class finish

--- src/credentialsName/Student.java
+++ src/credentialsName/Student.java
@@ -19,37 +19,153 @@ public class Student {
 	private String sNombre;
 	private String pApellido;
 	private String sApellido;
+	private String group;
+	
+	
+	/**
+	 *Default constructor 
+	 */
+	
+	public Student(){
+		
+	}
 	
 	/**
 	 * constructor for a person with two names.
-	 * @param String primerNombre;
-	 * @param String segundoNombre;
-	 * @param String apellidoPaterno;
-	 * @param String apellidoMaterno; 
+	 * @param String primerNombre
+	 * @param String segundoNombre
+	 * @param String apellidoPaterno
+	 * @param String apellidoMaterno
+	 * @param String group 
 	 */
 	
-	public Student(String pNombre, String sNombre, String pApellido, String sApellido){
+	public Student(String pNombre, String sNombre, String pApellido, String sApellido, String group){
 		this.pNombre = pNombre;
 		this.sNombre = sNombre;
 		this.pApellido = pApellido;
 		this.sApellido = sApellido;
+		this.group = group;
 	}
 	
 	/**
 	 * constructor for a person with one name.
-	 * @param String primerNombre;
-	 * @param String segundoNombre;
-	 * @param String apellidoMaterno; 
+	 * @param String primerNombre
+	 * @param String segundoNombre
+	 * @param String apellidoMaterno
+	 * @param String group
 	 */
 	
-	public Student(String pNombre, String pApellido, String sApellido){
+	public Student(String pNombre, String pApellido, String sApellido, String group){
 		this.pNombre = pNombre;
 		this.sNombre = "";
 		this.pApellido = pApellido;
 		this.sApellido = sApellido;
+		this.group = group;
+	}
+	
+	/**
+	 * Set the first name
+	 * @param String pName 
+	 */
+	
+	public void setPNombre(String pName){
+		this.pNombre = pName;
+	}
+	
+	/**
+	 * Set the second name
+	 * @param String sName 
+	 */
+	
+	public void setSNombre(String sName){
+		this.sNombre = sName;
+	}
+	
+	/**
+	 * Set the first last name
+	 * @param String pName 
+	 */
+	
+	public void setPApellido(String pApellido){
+		this.pApellido = pApellido;
+	}
+	
+	/**
+	 * Set the second last name
+	 * @param String sApellido 
+	 */
+	
+	public void setSApellido(String sApellido){
+		this.sApellido = sApellido;
 	}
 	
 	
+	/**
+	 * Set the group of an student.
+	 * @param String 
+	 */
 	
+	public void setGrupo(String group){
+		this.group = group;
+	}
+	
+	
+	/**
+	 * Get the first name
+	 * @return String
+	 */
+	
+	public String getPNombre(){
+		return pNombre;
+	}
+	
+	/**
+	 * Get the second name
+	 * @return String
+	 */
+	
+	public String getSNombre(){
+		return sNombre;
+	}
 
+	
+	/**
+	 * Get the first name
+	 * @return String
+	 */
+	
+	public String getPApellido(){
+		return pApellido;
+	}
+	
+	/**
+	 * Get the second name
+	 * @return String
+	 */
+	
+	public String getSApellido(){
+		return sApellido;
+	}
+	
+	/**
+	 * Getter method to know the group of an student
+	 * @return String
+	 */
+	
+	public String getGrupo(){
+		return group;
+	}
+	
+	/**
+	 * Method to String about an student
+	 * @return String
+	 */
+
+	@Override
+	public String toString() {
+		return "Student [pNombre=" + pNombre + ", sNombre=" + sNombre
+				+ ", pApellido=" + pApellido + ", sApellido=" + sApellido
+				+ ", group=" + group + "]";
+	}
+	
 }
